73bce699bb1cf865e8fc3c46f5816eb0df4de22f
chris williams <me@chrisw.gg>
2025-02-10 17:14:57 -0500

    Add mDNS service Avahi

--- hosts/starfield/network.nix
+++ hosts/starfield/network.nix
@@ -10,12 +10,27 @@
 
     firewall = {
       enable = true;
+      allowedTCPPorts = [
+        5353 # mDNS
+      ];
+      # Open a range of ports to use for locally hosted servers
       allowedTCPPortRanges = [
-        # Open a range of ports to use for locally hosted servers
         { from = 7000; to = 9000; }
       ];
     };
 
+  };
+
+  # mDNS service
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      # register mDNS for all local IP addresses
+      addresses = true;
+      openFirewall = false;
+    };
   };
 
 }
